b2ca59ed87027b68221b66aadd6983a38c5dfc73
JMarion2 <jmarion@siue.edu>
2024-03-31 22:45:39 -0500

    Wandering enemies, bullets destroyed offscreen

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
-    private float _speed; //Enemy speed.
+    private float _speed; // Enemy speed.
     [SerializeField]    
-    private float _rotationSpeed; //Enemy rotation speed.
+    private float _rotationSpeed; // Enemy rotation speed.
 
-    private Rigidbody2D _rigidbody; //Enemy rigidbody.
-    private PlayerAwarenessController _playerAwarenessController; //Go to 'PlayerAwarenessController' class.
+    private Rigidbody2D _rigidbody; // Enemy rigidbody.
+    private PlayerAwarenessController _playerAwarenessController; // Go to 'PlayerAwarenessController' class.
     private Vector2 _targetDirection;
+    private float _changeDirectionCooldown; // amount of time remaining until next direction change
 
     // Start is called before the first frame update
     private void Awake(){
 
-        //Gets rigid2D body component and assigns it the field.
+        // Gets rigid2D body component and assigns it the field.
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        _targetDirection = transform.up;
     }
 
     // Update is called once per frame
@@ -35,19 +37,32 @@ public class EnemyMovement : MonoBehaviour
     }
 
     private void UpdateTargetDirection(){
+        // Wander randomly unless detects player
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+        
+    }
 
+    private void HandleRandomDirectionChange() {
+        _changeDirectionCooldown -= Time.deltaTime;
+        // If cool down is complete
+        if (_changeDirectionCooldown <= 0) {
+            // create a random angle
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            _targetDirection = rotation * _targetDirection;
+            // Set cool down again after direction change complete
+            _changeDirectionCooldown = Random.Range(1f, 5f);
+        }
+    }
+
+    private void HandlePlayerTargeting() {
         if(_playerAwarenessController.AwareOfPlayer){
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
-        }else{
-            _targetDirection = Vector2.zero;
         }
-
     }
 
     private void RotateTowardsTarget(){
-        if(_targetDirection == Vector2.zero){
-            return;
-        }//If there is a target direction, we'll calculate the target's rotation.
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation,  targetRotation, _rotationSpeed * Time.deltaTime);
         
@@ -56,15 +71,7 @@ public class EnemyMovement : MonoBehaviour
     }
 
     private void SetVelocity(){
-
-        if(_targetDirection == Vector2.zero){ //Make sure target-direction hasn't already been set.
-            _rigidbody.velocity = Vector2.zero;
-        }else{//If we have a target direction.
-            _rigidbody.velocity = transform.up * _speed;
-        }
-
+        _rigidbody.velocity = transform.up * _speed;
     }
-
-
 
 }
--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Camera _camera;
+
+    private void Awake () {
+        _camera = Camera.main;
+    }
+
+    private void Update() {
+        DestroyWhenOffScreen();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.GetComponent<EnemyMovement>()){ 
@@ -13,6 +22,17 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject); // Remove Bullet
 
         }
+    }
+
+    private void DestroyWhenOffScreen() {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.x < 0 || 
+            screenPosition.x > _camera.pixelWidth ||
+            screenPosition.y < 0 ||
+            screenPosition.y > _camera.pixelHeight) {
+                Destroy(gameObject);
+            }
     }
 
 }
