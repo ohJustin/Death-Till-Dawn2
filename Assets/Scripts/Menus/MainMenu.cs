318e7f13ea0332e7401690e4a3f1b7680f59df5c
Justin <jbcool02@yahoo.com>
2024-04-23 21:05:58 -0500

    Added additional options to the main menu, including a re-usable menu concept. Check Discord For Notes.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+   public GameObject optionsWindow;
+  public GameObject aboutWindow;
 
-    public GameObject exitBtn;
-    public GameObject restartBtn;
+    // For our other buttons that involve a prompt window, i'll be using unity's 'Button'.
+
+    public TextMeshProUGUI messageText;
    
     void Start()
     {
-
+      //optionsWindow = GameObject.Find("OptionsWindow");
+      optionsWindow.SetActive(false);
+      aboutWindow.SetActive(false);
     }
 
-
+    public void ControlsClicked(){
+           Debug.Log("Options selected.");
+           optionsWindow.SetActive(true);
+    }
     // Restart game when restartBtn or restartBtnBack onClick is activated.
     public void StartGame() {
         Debug.Log("StartGame");
         SceneManager.LoadScene(1);
     }
+
+    public void DoneClicked(){
+        optionsWindow.SetActive(false);
+        aboutWindow.SetActive(false);
+        Debug.Log("Done selected.");
+    }
+   
+   
+    public void AboutClicked(){
+        aboutWindow.SetActive(true);
+        Debug.Log("About selected.");
+    }
+
+    public void QuitClicked(){
+        Application.Quit();
+    }
+
+   
+
+
 }
